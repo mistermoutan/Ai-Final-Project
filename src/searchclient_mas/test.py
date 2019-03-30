9123cfcec40521f2eaf0afe92c8315d3b9c9d390
Tord <s153083@student.dtu.dk>
2019-03-30 17:46:07 +0100

    added a simple test case for Gudni

---
+++ src/searchclient_mas/test.py
@@ -0,0 +1,16 @@
+import test_utilities as tu
+
+agt0 = tu.agent(0,"red")
+agt1 = tu.agent(1,"blue")
+level = [
+    [False, False,False,False],
+    [False, agt0,agt1,False],
+    [False, False,False,False]
+]
+s = tu.make_state(level)
+action = [tu.move(tu.east), tu.move(tu.west)]
+x = s.get_child(action)
+
+y = 123
+
+
