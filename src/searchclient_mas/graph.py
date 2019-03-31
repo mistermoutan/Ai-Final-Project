0461cbf89806df23be9deab36ff964a0658888b7
MrMoutan <s182312@student.dtu.dk>
2019-03-31 14:19:31 +0200

    graph

--- src/searchclient_mas/graph.py
+++ src/searchclient_mas/graph.py
@@ -1,4 +1,3 @@
-import state
 import numpy as np
 from collections import deque
 from action import *
@@ -8,58 +7,51 @@ from collections import defaultdict
 Converts Level to Graph (only non walls cells are considered)
 
     Constructor:
-        - Requires an instance of StateMA or StateSA
-        - The precompute parameter allows:
 
-                - Precompute distance from all vertices of graph to each goal 
+        - Requires an instance of StateMA or StateSA
+        
+        The precompute parameter allows for:
+
+                - Precompute distance from all vertices of graph to each goal
                 - Precompute distance from all boxes to each goal
 
 
     Methods
-     - BFS_ShortestPath : Returns shortest path between a source vertice and target vertice in a deque with directions on the grid (N,S,W,E) (deques can be indexed as lists)
-     - getNeighbours: Get neighbouring vertices of a particular vertice
+
+     - BFS_ShortestPath : Returns shortest path between a source vertex and target vertex in a deque with directions on the grid (N,S,W,E) (deques can be indexed as lists)
+     - getNeighbours: Get neighbouring vertices of a particular vertex
      - BackTrack: Used to return the shortest path in terms of directions, called once BFS finds a solution
 
 """
 class Graph (object) :
 
-    def __init__(self,state, precompute = None):
+    def __init__(self,state,precompute = None):
 
         row,col = np.asarray(state.maze).shape
-        self.vertices = {(i,j) for i in range(row) for j in range(col) if maze[i][j] == True}     #vertices of graph representation of level
+        self.vertices = {(i,j) for i in range(row) for j in range(col) if maze[i][j] == True}     
 
-        # build dict of shortest paths from all vertices of graph to the goal vertices
-        if "all_to_goals" in precompute.split():
-            self.shortest_path_to_goals = defaultdict(list) #keys: goals ; values: tuple(vertice,shortest past from that vertice to goal)
-            #self.goals = set(state.goal_positions)
+        #dict of shortest paths from all vertices of graph to the goal vertices
+        if "all_to_goals" in precompute:
+            self.shortest_path_to_goals = defaultdict(list) # goals:(vertex,shortest past from that vertex to goal)
 
             for goal_position in state.goal_positions:
-                for vertice in self.vertices if vertice not in self.goals:  #for all vertices that are not goals
-                    shortest_path = self.BFS_ShortestPath(vertice,goal_position)
-                    if shortest_path:
-                        self.shortest_path_to_goals[goal_position].append((vertice,shortest_path))
-                    else:
-                        # if there is no path
-                        self.shortest_path_to_goals[goal_position].append((vertice,None)) 
+                for vertex in self.vertices if vertex not in self.goals:  #for all vertices that are not goals
+                    shortest_path = self.BFS_ShortestPath(vertex,goal_position)
+                        self.shortest_path_to_goals[goal_position].append((vertex,shortest_path))
+                    
         
         # build dict of shortest path from each box to all goals
-        elif "boxes_to_goals" in precompute.split():
-            self.shortest_path_boxes_to_goals = defaultdict(list) # keys:goals ; values: tuple(box_position,shortest past from that vertice to goal)
-            #self.goals = set(state.goal_positions)
-            #self.boxes = set(state.box_positions)
+        elif "boxes_to_goals" in precompute:
+            self.shortest_path_boxes_to_goals = defaultdict(list) #goals (box_position,shortest past from that vertex to goal)
 
             for goal_position in state.goal_positions:
                 for box_positon in state.box_positions:
                     shortest_path = self.BFS_ShortestPath((box_positon,goal_position))
-                    if shortest_path:
                         self.shortest_path_boxes_to_goals[goal_position].append((box_positon,shortest_path))
-                    else:
-                        self.shortest_path_to_goals[goal_position].append((box_positon,None))
 
+    def getNeighbours(self,vertex):
 
-    def getNeighbours(self,vertice):
-
-        (x,y) = vertice
+        (x,y) = vertex
         neighbours = {(x,y+1),(x,y-1),(x-1,y),(x+1,y)}
         neighbours = {n for n in neighbours if n in self.vertices}
 
@@ -73,13 +65,12 @@ class Graph (object) :
 
         #get path, composed of vertices
         while parent != source_vertice:
-            path.insert(0,parent) # appent parent to beggining of path array
-            parent = parent_dict[parent]  # get next parent
+            path.insert(0,parent) 
+            parent = parent_dict[parent] 
 
-        #still missing the source vertice (it has no parent)
+        #still missing the source vertex (it has no parent)
         path.insert(0,source_vertice)
 
-        #turn path into directions:
         directions = deque()
         for i in range(len(path) - 1):
 
@@ -99,14 +90,14 @@ class Graph (object) :
         return directions
 
     def BFS_ShortestPath (self,source_vertice,target_vertice):
-        """ Returns Shortest in terms of  S,E,W,N directions"""
+        """ Returns Shortest in terms of S,E,W,N directions"""
 
-        assert source_vertice in self.vertices and target_vertice in self.vertices, "Insert coordinates that are part of the level"
+        assert source_vertice in self.vertices and target_vertice in self.vertices, "Insert coordinates that are part of the state"
         
         queue = deque() 
-        queue.append(source_vertice) #adds to the right
+        queue.append(source_vertice) 
         explored_set = set()
-        parent = {} #children/neighbours as keys and their parent vertice as values
+        self.parent = {} # neighbours:parent 
         
 
         while len(queue) > 0:
@@ -126,22 +117,25 @@ class Graph (object) :
                     queue.append(neighbour)
 
             if len(queue) == 0:
-                print("Failed to find Shortest path, make sure there is a path between the source and target vertice")
+                print("Failed to find Shortest path")
                 return None
 
 
 
             
 
-
+"""
 maze = [[True,True,False,True],
         [True,True,True,True],
         [True,False,False,True],
         [True,False,True,True]]
 
+
+
 g = Graph(maze)
 g.BFS_ShortestPath((3,2),(3,0))
 
+"""
 
 
 
