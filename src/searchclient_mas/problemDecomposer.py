e717e967c5d22e854b269bbbe35f2d5eb75a1515
Eike <ek@eikes-mbp.eduroam.wireless.dtu.dk>
2019-04-04 08:25:37 +0200

    changes in problemDecomposer

--- src/searchclient_mas/problemDecomposer.py
+++ src/searchclient_mas/problemDecomposer.py
@@ -25,8 +25,11 @@ class problemDecomposer():
     '''Checks if all boxes are at their goal state
     returns list of boolean where true represents a need for a task
     '''
-    def checkGoals(self):
-        pass
+    def getGoalOrientedProblems(self):
+        
+        g = []
+        for i in range(len(self.state.goal_types)):
+            g.append((i,self.searchPossibleBoxesForGoal(i)))
 
         '''self.goal_need_task =[]
         #iterate over all goals:
@@ -40,22 +43,15 @@ class problemDecomposer():
             else:
                 self.goal_need_task[i]=True
     '''
-    '''Multiple agents for one box possible?'''
     def searchPossibleAgentsForBox(self,box_idx):
-        '''returns a list of lists of agent indexes that are able to move the boxes at pos i in the box list'''
+        '''returns a list of agent indexes that are able to move the box at pos box_idx in the box list'''
         return [idx for idx in range(len(self.state.agent_colors)) if self.state.box_colors[box_idx]==self.state.agent_color[idx]]
-    def searchPossibleBoxesForGoals(self):
-        '''returns a list of lists of box-indexes that are able to satisfy the goal at pos i in the goal list'''
-        self.pos_boxes=[]
-        for i in range(len(self.state.goal_types)):
-            self.pos_boxes.append([idx for idx in range(len(self.state.box_types)) if self.state.goal_types[i] ==self.state.box_types[idx]])
-                
-    def searchPossibleGoalsForBoxes(self):
-        '''returns a list of lists of goal-indexes that are able to store the box at pos i in the goal list'''
-        self.pos_goals=[]
-        for i in range(len(self.state.box_types)):
-            self.pos_goals.append([idx for idx in range(len(self.state.goal_types)) if self.state.goal_types[idx] ==self.state.box_types[i]])
-        
+    def searchPossibleBoxesForGoal(self,goal_idx):
+        '''returns a list of box-indexes that are able to satisfy the goal at pos goad_idx in the goal list'''
+        return [idx for idx in range(len(self.state.box_types)) if self.state.goal_types[goal_idx] ==self.state.box_types[idx]]
+    def searchPossibleGoalsForBox(self,box_idx):
+        '''returns a list of goal-indexes that are able to store the box at index box_idx'''
+        return [idx for idx in range(len(self.state.goal_types)) if self.state.goal_types[idx] ==self.state.box_types[box_idx]]
     def getTasks(self):
         return self.Tasks
  
