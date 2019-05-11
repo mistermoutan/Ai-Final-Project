f3616aa23e3bb6025be615dee0e9e32263d7d830
drulludanni <drulludanni5@gmail.com>
2019-05-11 21:01:46 +0200

    fixed small issue

--- src/searchclient_mas/Parallel_planner.py
+++ src/searchclient_mas/Parallel_planner.py
@@ -38,8 +38,15 @@ class PathNode:
 
 class ParallelPlanner:
     def __init__(self, state: StateMA):
-        self.initial_state = state
-        self.state = state.copy()
+        self.state = state
+        self.level_analyzer = LevelAnalyser(state)
+
+        while self.level_analyzer.immovable_boxes:
+            self.state.remove_immovable_boxes(self.level_analyzer.immovable_boxes)
+            self.level_analyzer = LevelAnalyser(self.state)
+
+        print(state)
+        self.initial_state = state.copy()
         self.goal_analyzer = GoalAnalyzer(state)
         self.current_plan = []
         self.blocked = set()
@@ -48,11 +55,6 @@ class ParallelPlanner:
         self.unusable_boxes = set()
         self.unusable_agents = set()
         self.dist = DistanceComputer(self.state)
-        self.level_analyzer = LevelAnalyser(state)
-        
-        while self.level_analyzer.immovable_boxes:
-            self.state.remove_immovable_boxes(self.level_analyzer.immovable_boxes)
-            self.level_analyzer = LevelAnalyser(self.state)
 
 
     def find_boxes_and_agents_for_goal(self, goal_id):
