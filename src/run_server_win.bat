e608a8e7071661c3a17cd7e7416f6a9c4bcc68fb
drulludanni <drulludanni5@gmail.com>
2019-05-23 12:07:17 +0200

    minor changes and added comp19 levels

--- src/run_server_win.bat
+++ src/run_server_win.bat
@@ -1,1 +1,1 @@
-java -jar server.jar -c "python searchclient_mas/searchclient.py -par" -l "comp17/SAgroupname.lvl" -g
+java -jar server.jar -c "python searchclient_mas/searchclient.py -par" -l "comp17/SALiquorice.lvl" -g
